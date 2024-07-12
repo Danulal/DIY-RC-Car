15889e7b47f93b3b578257dd07e30e2c538fb07a
Daniel Größinger <daniel.groessinger@gmail.com>
2024-07-13 00:26:24 +0200

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,39 @@
 #include <Arduino.h>
-#include "../lib/Bluetooth/bluetooth.h"
 
-static constexpr int PWM_STEER = 5;
-static constexpr int PWM_ACCELL = 6;
-char btVal;
-char btSignal[7];
+#include "motorctrl.h"
+#include "elrs.h"
 
-void setup() {
+static constexpr int PWM_STEER = 5; // define motor pins for steering and accelerating
+static constexpr int PWM_ACCEL = 6;
+static constexpr int STEER_REVERSE = 7;
+static constexpr int ACCEL_REVERSE = 10;
+// static constexpr int LED = ?;
+
+static constexpr int CH_STEER = 1; // define elrs receiver channels       
+static constexpr int CH_THROTTLE = 3;
+static constexpr int CH_REVERSE = 10;
+static constexpr int CH_ARM = 6;
+static constexpr int CH_BRAKE = 9;
+static constexpr int CH_LED = 8;
+
+int throttle;
+int steering;
+
+void setup()
+{
   pinMode(PWM_STEER, OUTPUT);
-  pinMode(PWM_ACCELL, OUTPUT);
-  Serial.begin(9600);
+  pinMode(PWM_ACCEL, OUTPUT);
+  init_elrs(115200);
 }
 
-void loop() {
-  if(Serial.available()){
-    readBT(btSignal);
-  }
+// Use crsf.getChannel(x) to get us channel values (1-16).
 
-  if(btVal == '1'){
-    analogWrite(PWM_STEER, 255);
-  }
-  else if(btVal == '2'){
-    analogWrite(PWM_STEER, 0);
-  }
+void loop()
+{
+  // Must call update_elrs() in loop() to process data
+  update_elrs();
+  throttle = readCH(CH_THROTTLE);
+  steering = readCH(CH_STEER);
+  printChannels();
+  // accel(PWM_ACCEL, throttle);
 }
-
