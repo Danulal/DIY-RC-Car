787a16163bf63c0655075677a1dab22057ca4249
Daniel Größinger <daniel.groessinger@gmail.com>
2024-07-14 20:43:17 +0200

    update headers to prevent double includes

--- lib/elrs/elrs.h
+++ lib/elrs/elrs.h
@@ -1,5 +1,8 @@
+#ifndef ELRS
+#define ELRS
 void init_elrs(long int baudrate);
 void update_elrs();
 int readCH(int channel);
 bool elrs_2way_switch(int channel);
 void printChannels();
+#endif // ELRS
--- lib/motorctrl/motorctrl.h
+++ lib/motorctrl/motorctrl.h
@@ -1,3 +1,6 @@
+#ifndef MOTORCTRL
+#define MOTORCTRL
 void accel(int pwm_pin, int reverse_pin, int throttle, bool reversed);
 void steer(int pwm_pin, int reverse_pin, int deflection);
 void drive_reverse(int reverse_pin, int pwm_pin, int current_pwm, bool reversed);
+#endif // MOTORCTRL
