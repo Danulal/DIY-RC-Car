b867689d4a2a333587f830d41334c19892249cc9
Daniel Größinger <daniel.groessinger@gmail.com>
2024-07-15 17:27:47 +0200

    add elrs connection detection with timeout

--- lib/elrs/elrs.cpp
+++ lib/elrs/elrs.cpp
@@ -41,3 +41,37 @@ void printChannels() { // debug for printing out all channels of radiomaster zor
     Serial.print(", ");
     Serial.println(" ");
 }
+
+unsigned long start_millis_elrs = 0; // variables needed for the timer in is_elrs_link_up()
+unsigned long current_millis_elrs = 0;
+bool previous_link_state = 0;
+
+bool is_elrs_link_up(unsigned long timeout) {
+
+    // Serial.print("isLinkUp: "); // timer debug
+    // Serial.print(crsf.isLinkUp());
+    // Serial.print("\tprevious link state: ");
+    // Serial.print(previous_link_state);
+    // Serial.print("\tcurrent_millis: ");
+    // Serial.print(current_millis_elrs);
+    // Serial.print("\tstart_millis: ");
+    // Serial.print(start_millis_elrs);
+    // Serial.print("\n");
+
+    current_millis_elrs = millis();
+
+    if(crsf.isLinkUp() == 0 && previous_link_state == 1) {
+        if(start_millis_elrs == current_millis_elrs) {
+            start_millis_elrs = millis();
+        }
+        if(current_millis_elrs - start_millis_elrs >= timeout) {
+            previous_link_state = 0;
+            start_millis_elrs = current_millis_elrs;
+        }
+    } else if(crsf.isLinkUp() == 1) {
+        previous_link_state = 1;
+        start_millis_elrs = current_millis_elrs;
+    }
+
+    return previous_link_state;
+}
--- lib/elrs/elrs.h
+++ lib/elrs/elrs.h
@@ -4,5 +4,6 @@ void init_elrs(long int baudrate);
 void update_elrs();
 int readCH(int channel);
 bool elrs_2way_switch(int channel);
+bool is_elrs_link_up(unsigned long timeout);
 void printChannels();
 #endif // ELRS
